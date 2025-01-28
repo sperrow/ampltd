6f067d026ebc6db4ce6665798fcb7f0560d79132
Matthew Sperry <mattjsperry@gmail.com>
2025-01-28 01:57:30 -0800

    add logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import Image from 'next/image';
@@ -7,8 +6,8 @@ import { lusitana } from '@/app/ui/fonts';
 export default function Page() {
     return (
         <main className="flex min-h-screen flex-col p-6">
-            <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-                <AcmeLogo />
+            <div className="flex h-20 shrink-0 items-end rounded-lg bg-lime-200 p-4 md:h-52">
+                <Image src="/alma_logo.png" width={200} height={50} alt="Alma Logo" />
             </div>
             <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
                 <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
