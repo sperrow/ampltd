f4ed3cd4a5cbe09427bfc20f411c9009d6ec3510
Matthew Sperry <mattjsperry@gmail.com>
2025-01-27 22:33:46 -0800

    db schema

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -1,147 +1,78 @@
 // This file contains placeholder data that you'll be replacing with real data in the Data Fetching chapter:
 // https://nextjs.org/learn/dashboard-app/fetching-data
 const users = [
-  {
-    id: '410544b2-4001-4271-9855-fec4b6a6442a',
-    name: 'User',
-    email: 'user@nextmail.com',
-    password: '123456',
-  },
+    {
+        id: '410544b2-4001-4271-9855-fec4b6a6442a',
+        name: 'User',
+        email: 'user@nextmail.com',
+        password: '123456',
+    },
 ];
 
-const customers = [
-  {
-    id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81aa',
-    name: 'Evil Rabbit',
-    email: 'evil@rabbit.com',
-    image_url: '/customers/evil-rabbit.png',
-  },
-  {
-    id: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
-    name: 'Delba de Oliveira',
-    email: 'delba@oliveira.com',
-    image_url: '/customers/delba-de-oliveira.png',
-  },
-  {
-    id: '3958dc9e-742f-4377-85e9-fec4b6a6442a',
-    name: 'Lee Robinson',
-    email: 'lee@robinson.com',
-    image_url: '/customers/lee-robinson.png',
-  },
-  {
-    id: '76d65c26-f784-44a2-ac19-586678f7c2f2',
-    name: 'Michael Novotny',
-    email: 'michael@novotny.com',
-    image_url: '/customers/michael-novotny.png',
-  },
-  {
-    id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C144B9',
-    name: 'Amy Burns',
-    email: 'amy@burns.com',
-    image_url: '/customers/amy-burns.png',
-  },
-  {
-    id: '13D07535-C59E-4157-A011-F8D2EF4E0CBB',
-    name: 'Balazs Orban',
-    email: 'balazs@orban.com',
-    image_url: '/customers/balazs-orban.png',
-  },
+const leads = [
+    {
+        id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81aa',
+        name: 'Jorge Ruiz',
+        country: 'Mexico',
+        date: '2023-03-15',
+        status: 'PENDING',
+    },
+    {
+        id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81aa',
+        name: 'Bahar Zamir',
+        country: 'Mexico',
+        date: '2022-08-22',
+        status: 'PENDING',
+    },
+    {
+        id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81ac',
+        name: 'John Doe',
+        country: 'Japan',
+        date: '2023-11-05',
+        status: 'PENDING',
+    },
+    {
+        id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81ad',
+        name: 'Jane Doe',
+        country: 'Russia',
+        date: '2023-01-19',
+        status: 'PENDING',
+    },
+    {
+        id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81ae',
+        name: 'Bob Smith',
+        country: 'Canada',
+        date: '2023-06-30',
+        status: 'PENDING',
+    },
+    {
+        id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81af',
+        name: 'Alice Johnson',
+        country: 'Canada',
+        date: '2022-02-25',
+        status: 'REACHED_OUT',
+    },
+    {
+        id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81b0',
+        name: 'Eve Jackson',
+        country: 'Brazil',
+        date: '2023-09-10',
+        status: 'PENDING',
+    },
+    {
+        id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81b1',
+        name: 'Carol Davis',
+        country: 'South Korea',
+        date: '2023-04-14',
+        status: 'PENDING',
+    },
+    {
+        id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81b2',
+        name: 'David Lee',
+        country: 'Mexico',
+        date: '2022-11-28',
+        status: 'PENDING',
+    },
 ];
 
-const invoices = [
-  {
-    customer_id: customers[0].id,
-    amount: 15795,
-    status: 'pending',
-    date: '2022-12-06',
-  },
-  {
-    customer_id: customers[1].id,
-    amount: 20348,
-    status: 'pending',
-    date: '2022-11-14',
-  },
-  {
-    customer_id: customers[4].id,
-    amount: 3040,
-    status: 'paid',
-    date: '2022-10-29',
-  },
-  {
-    customer_id: customers[3].id,
-    amount: 44800,
-    status: 'paid',
-    date: '2023-09-10',
-  },
-  {
-    customer_id: customers[5].id,
-    amount: 34577,
-    status: 'pending',
-    date: '2023-08-05',
-  },
-  {
-    customer_id: customers[2].id,
-    amount: 54246,
-    status: 'pending',
-    date: '2023-07-16',
-  },
-  {
-    customer_id: customers[0].id,
-    amount: 666,
-    status: 'pending',
-    date: '2023-06-27',
-  },
-  {
-    customer_id: customers[3].id,
-    amount: 32545,
-    status: 'paid',
-    date: '2023-06-09',
-  },
-  {
-    customer_id: customers[4].id,
-    amount: 1250,
-    status: 'paid',
-    date: '2023-06-17',
-  },
-  {
-    customer_id: customers[5].id,
-    amount: 8546,
-    status: 'paid',
-    date: '2023-06-07',
-  },
-  {
-    customer_id: customers[1].id,
-    amount: 500,
-    status: 'paid',
-    date: '2023-08-19',
-  },
-  {
-    customer_id: customers[5].id,
-    amount: 8945,
-    status: 'paid',
-    date: '2023-06-03',
-  },
-  {
-    customer_id: customers[2].id,
-    amount: 1000,
-    status: 'paid',
-    date: '2022-06-05',
-  },
-];
-
-const revenue = [
-  { month: 'Jan', revenue: 2000 },
-  { month: 'Feb', revenue: 1800 },
-  { month: 'Mar', revenue: 2200 },
-  { month: 'Apr', revenue: 2500 },
-  { month: 'May', revenue: 2300 },
-  { month: 'Jun', revenue: 3200 },
-  { month: 'Jul', revenue: 3500 },
-  { month: 'Aug', revenue: 3700 },
-  { month: 'Sep', revenue: 2500 },
-  { month: 'Oct', revenue: 2800 },
-  { month: 'Nov', revenue: 3000 },
-  { month: 'Dec', revenue: 4800 },
-];
-
-export { users, customers, invoices, revenue };
+export { users, leads };
